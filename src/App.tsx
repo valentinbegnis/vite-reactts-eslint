d1faa61a8f05002e929de9f119b1b676cad5e1bc
valentinbegnis <begnisvalentin@gmail.com>
2023-01-18 15:33:21 -0300

    First boilerplate version

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import typescriptLogo from './assets/typescript.svg'
+import eslintLogo from './assets/eslint.svg'
 import './App.css'
 
-function App() {
+function App (): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://www.typescriptlang.org/docs/" target="_blank" rel="noreferrer">
+          <img src={typescriptLogo} className="logo typescript" alt="TypeScript logo" />
+        </a>
+        <a href="https://eslint.org/docs/latest/" target="_blank" rel="noreferrer">
+          <img src={eslintLogo} className="logo eslint" alt="ESLint logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TypeScript + ESLint 🚀</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => { setCount((count) => count + 1) }}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="notes">
+        Do not forget to install the ESLint extension on VSCode.
+        <br />
+        I hope this boilerplate helps.
       </p>
     </div>
   )
